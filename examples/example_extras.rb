1cdbf988b9fa37218eba1a32e6f72fa2b2b1f78c
YukiJikumaru <jikumaruyuki@gmail.com>
2022-10-10 16:32:17 +0900

    Rename example=nested.rb to example_extras.rb

--- examples/example_extras.rb
+++ examples/example_extras.rb
@@ -3,20 +3,20 @@
 require 'dry-types'
 require 'dry-struct'
 
-class ExampleNested < ::Dry::Struct
+class ExampleOthers < ::Dry::Struct
   module Types
     include ::Dry.Types()
   end
 
-  class Fooo < ::Dry::Struct
+  class InnerKlass < ::Dry::Struct
     attribute :string, Types::String
   end
 
-  attribute? :instance1, Types.Instance(Fooo)
+  attribute? :instance1, Types.Instance(InnerKlass)
   attribute? :instance2, Types.Instance(::Range)
   attribute? :instance3, Types.Instance(::Set)
 
-  attribute? :constructor1, Types.Constructor(Fooo)
+  attribute? :constructor1, Types.Constructor(InnerKlass)
   attribute? :constructor2, Types.Constructor(::Range)
   attribute? :constructor3, Types.Constructor(::Set)
 
